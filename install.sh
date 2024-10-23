b1192ff2e5dd2e8e4b87861133e7ee204641bb61
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2024-10-23 14:07:55 +0200

    basic script for installation and execution

---
+++ install.sh
@@ -0,0 +1,43 @@
+sudo apt update && sudo apt install -y tmux qtcreator qtbase5-dev libqt5serialport5-dev qtmultimedia5-dev libqt5gamepad5-dev libunwind-dev libqt5serialport5-dev git build-essential cmake python3-colcon-common-extensions
+mkdir ~/Sunrise
+cd ~/Sunrise
+
+mkdir carla
+cd carla
+# wget carla/.tar
+# tar -xvf carla/0.9..
+cd ..
+
+# carla-ros-bridge
+#
+mkdir -p carla-ros-bridge/src
+cd carla-ros-bridge/src
+git clone --recurse-submodules git@github.com:RISE-Dependable-Transport-Systems/carla-ros-bridge.git
+cd ../
+rosdep install -i --from-path src --rosdistro humble -r -y
+pip3 install -r src/carla-ros-bridge/requirements.txt
+colcon build --symlink-install --packages-skip rviz_carla_plugin carla_ad_demo pcl_recorder
+source ./install/local_setup.bash
+
+# waywiser
+#
+
+#wget https://github.com/mavlink/MAVSDK/releases/download/v2.0.0/libmavsdk-dev_2.0.0_ubuntu22.04_amd64.deb
+#sudo dpkg -i libmavsdk-dev*.deb
+
+mkdir -p ~/waywiser/src
+cd ~/waywiser/src
+git clone --recurse-submodules git@github.com:RISE-Dependable-Transport-Systems/WayWiseR.git
+cd ..
+rosdep install -i --from-path src --rosdistro humble -r -y
+colcon build --symlink-install
+
+# control tower
+#
+#
+cd ..
+git clone --recursive git@github.com:RISE-Dependable-Transport-Systems/ControlTower.git
+cd ControlTower
+mkdir build && cd build
+cmake ..
+cmake --build . --parallel
---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/zsh
+# Check if tmux is installed
+if ! command -v tmux &>/dev/null; then
+  echo "tmux is not installed. Install it first."
+  sudo apt install -y tmux
+fi
+# Define the tmux session name
+SESSION_NAME="carla_sunrise"
+
+# Check if the session already exists
+tmux has-session -t $SESSION_NAME 2>/dev/null
+
+if [ $? != 0 ]; then
+  # Create a new tmux session
+  #
+  export MAIN_FOLDER=$PWD
+  export CARLA_ROOT=$MAIN_FOLDER/carla_fromSrc
+  export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.15-py3.10-linux-x86_64.egg:$CARLA_ROOT/PythonAPI/carla
+
+  cd $MAIN_FOLDER/carla_ros_bridge/
+  source install/setup.zsh
+  cd $MAIN_FOLDER/waywiser
+  source install/setup.zsh
+
+  cd $MAIN_FOLDER
+  tmux new -d -s $SESSION_NAME
+
+
+  tmux send-keys -t $SESSION_NAME "$CARLA_ROOT/CarlaUE4.sh -carla-streaming-port=0" C-m
+  sleep 5
+
+  tmux new-window -t $SESSION_NAME
+  
+
+  tmux send-keys -t $SESSION_NAME "ros2 launch waywiser_carla carla.launch.py" C-m
+  tmux split-window -v -t $SESSION_NAME
+  sleep 5
+
+  tmux send-keys -t $SESSION_NAME "ros2 launch waywiser_carla waywiser_carla_relay.launch.py" C-m
+  tmux split-window -h -t $SESSION_NAME
+
+  tmux send-keys -t $SESSION_NAME "ros2 launch waywiser_hwbringup waywise_truck_autopilot.launch.py" C-m
+  sleep 5
+
+  tmux select-pane -t 1
+  tmux split-window -h -t $SESSION_NAME
+  tmux send-keys -t $SESSION_NAME "ros2 launch waywiser_carla carla_osm_tile_server.launch.py" C-m
+  
+  tmux split-window -v -t $SESSION_NAME
+  tmux send-keys -t $SESSION_NAME "$MAIN_FOLDER/ControlTower/build/ControlTower" C-m
+
+
+  tmux new-window -t $SESSION_NAME
+  tmux send-keys -t $SESSION_NAME "ros2 launch waywiser_twist_safety twist_safety.launch.py use_sim_time:=true" C-m
+
+  tmux split-window -h -t $SESSION_NAME
+  tmux send-keys -t $SESSION_NAME "ros2 topic pub --once /emergency_stop/target_state waywiser_twist_safety/msg/EmergencyStopState \"{sender_id : 'command_line' , state : 1}\"" C-m
+  tmux attach-session -t carla_sunrise 
+
+fi
